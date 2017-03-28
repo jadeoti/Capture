f10a78038692a9b4590774f338b4db92018be867
Adedeji Adeoti <adedejiadeoti@gmail.com>
2017-03-28 23:58:56 +0100

    uploaded image to server

--- app/src/main/java/com/jadeoti/capture/services/DataUploadService.java
+++ app/src/main/java/com/jadeoti/capture/services/DataUploadService.java
@@ -4,7 +4,9 @@ import android.app.IntentService;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
 
+import com.google.gson.Gson;
 import com.jadeoti.capture.BuildConfig;
 import com.jadeoti.capture.data.model.Person;
 import com.jcraft.jsch.Channel;
@@ -14,6 +16,9 @@ import com.jcraft.jsch.Session;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Locale;
 
 import timber.log.Timber;
 
@@ -50,13 +55,28 @@ public class DataUploadService extends IntentService{
         Person person = (Person) extras.getSerializable("profile");
         if(person == null) return;
 
+        //write person to fil
 
+        File textFile = new File("");
+        try {
+            File root = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), "Profiles");
+            // if directory exists and folder with name Profiles
+            if (!root.exists()) {
+                root.mkdirs(); // this will create folder.
+            }
+            String filename = String.format(Locale.getDefault(), "%s_%s.txt", person.getFirstName(), person.getSurname());
+            textFile = new File(root, filename);  // file path to save
+            FileWriter writer = new FileWriter(textFile);
+            String data = new Gson().toJson(person, Person.class);
+            writer.append(data);
+            writer.flush();
+            writer.close();
+            Timber.d("commit: %s successfully created", filename);
+        }catch (IOException ioe){
+            ioe.printStackTrace();
+            Timber.d("commit: failed to create document file");
 
-
-
-        String filePath = person.getImageUri(); //person.getFirstName() + "_" + person.getSurname() + ".txt";// get file path
-
-        Timber.d("pushing file %s", filePath);
+        }
 
 
         //TODO:
@@ -86,8 +106,12 @@ public class DataUploadService extends IntentService{
 
             channelSftp = (ChannelSftp)channel;
             channelSftp.cd(SFTPWORKINGDIR);
-            File f = new File(filePath);
-            channelSftp.put(new FileInputStream(f), f.getName());
+            // upload image
+            File imageFile = new File(person.getImageUri());
+            channelSftp.put(new FileInputStream(imageFile), imageFile.getName());
+
+            // upload text file
+            channelSftp.put(new FileInputStream(textFile), textFile.getName());
         }catch(Exception ex){
             ex.printStackTrace();
         }
